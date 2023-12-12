87fb022a3a00405a010f16b9e9a1047fcc62fdb0
Mohabkamel1 <mohabwael896@gmail.com>
2023-12-12 11:27:18 -0800

    18

---
+++ getlinee.c
@@ -0,0 +1,168 @@
+#include "shell.h"
+
+/**
+ * i_buf - buffer chained
+ * @info: input
+ * @buf: input
+ * @len: input
+ * Return: number
+ */
+
+ssize_t i_buf(info_t *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len)
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, siginHandler);
+
+		r = getlinee(buf, &len_p, stdin);
+
+		r = _getlinee(info, buf, &len_p);
+
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0';
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comment(*buf);
+			build_history_ls(info, *buf, info->histcount++);
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
+
+/**
+ * g_input - line minus a new line
+ * @info: input
+ * Return: number
+ */
+
+ssize_t g_input(info_t *info)
+{
+	static char *buf;
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->arg), *p;
+
+	_putchar(BUF_FLUSH);
+	r = i_buf(info, &buf, &len);
+	if (r == -1)
+		return (-1);
+	if (len)
+	{
+		j = i;
+		p = buf + i;
+
+		check_chain(info, buf, &j, i, len);
+		while (j < len)
+		{
+			if (i_chain(info, buf, &j))
+				break;
+			j++
+		}
+
+		i = j + 1;
+		if (i >= len)
+		{
+			i = len = 0;
+			info->cmd_buf_type = CMD_NORM;
+		}
+
+		*buf_p = p;
+		return (_strlength(p));
+	}
+
+	*buf_p = buf;
+	return (r);
+}
+
+/**
+ * read_buf - read a buffer
+ * @info: input
+ * @buf: input
+ * @i: input
+ * Return: r
+ */
+
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+
+/**
+ * _getlinee - get next line
+ * @info: input
+ * @ptr: input
+ * @length: input
+ * Return: s
+ */
+
+int _getlinee(info_t *info, char **ptr, size_t *length)
+{
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _strchar(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p)
+		return (p ? free(p), -1 : -1);
+
+	if (s)
+		_strconcat(new_p, buf + i, k - i);
+	else
+		_strcppy(new_p, buf + i, k - i + 1);
+
+	s += k - i;
+	i = k;
+	p = new_p;
+
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
+
+/**
+ * sigintHandler - block ctrl-c
+ * @sig_num: input
+ * Return: input
+ */
+
+void sigintHandler(_attribute_((unused)) int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
